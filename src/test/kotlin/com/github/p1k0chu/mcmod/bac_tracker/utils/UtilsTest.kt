fc814232289efebebcccfe1c2341a4c83500cec1
p1k0chu <anatoliyid0207@gmail.com>
2025-03-16 23:56:35 +0200

    test(Utils): add tests for 3 functions in Utils.kt

--- src/test/kotlin/com/github/p1k0chu/mcmod/bac_tracker/utils/UtilsTest.kt
+++ src/test/kotlin/com/github/p1k0chu/mcmod/bac_tracker/utils/UtilsTest.kt
@@ -1,7 +1,7 @@
 package com.github.p1k0chu.mcmod.bac_tracker.utils
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import com.google.api.services.sheets.v4.model.ValueRange
+import kotlin.test.*
 
 class UtilsTest {
     @Test
@@ -11,5 +11,41 @@ class UtilsTest {
         assertEquals("A4:Z", Utils.moveRangeDownBy("A1:Z", 3))
 
         assertEquals(Utils.moveRangeDownBy("bla", 1), null)
+    }
+
+    @Test
+    fun getIdOrUrlValidUrl() {
+        val validUrl = "https://docs.google.com/spreadsheets/d/MY_ID_HERE/edit?flags=useless"
+        assertEquals("MY_ID_HERE", Utils.getIdOrUrl(validUrl))
+    }
+
+    @Test
+    fun getIdOrUrlNotUrl() {
+        val input = "some id"
+        assertEquals(input, Utils.getIdOrUrl(input))
+    }
+
+    @Test
+    fun getProfilePictureByUuid() {
+        assertIs<String>(Utils.getProfilePictureByUuid("some uuid"))
+    }
+
+    @Test
+    fun getProfilePictureByUuidNull() {
+        assertNull(Utils.getProfilePictureByUuid(null))
+    }
+
+    @Test
+    fun singleColumnValueRange() {
+        val someData: List<List<String>> = listOf(
+            listOf("1"),
+            listOf("2"),
+            listOf("3")
+        )
+
+        val valueRange = ValueRange()
+        valueRange.setValues(someData)
+
+        assertEquals(listOf("1", "2", "3"), Utils.singleColumnValueRange(valueRange))
     }
 }
